1623553635d23bcf0cbd9bd731c5bda46575a85d
122344AS <143315452+122344AS@users.noreply.github.com>
2023-10-04 23:47:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /salman_Khan
 fi
-cd /Auto-search-tamil-bot
+cd /salman_Khan
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
